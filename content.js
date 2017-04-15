2bda4b231a36dbd10a3efac6a51c0f08f5ebaaf3
Adrien Joly <adrien@whyd.com>
2017-04-15 22:08:15 +0200

    clicking on recipient opens a google contacts search page for it

--- content.js
+++ content.js
@@ -1,34 +1,12 @@
+function attachLinks() {
+	document.querySelectorAll('div[email]').forEach(function(div){
+		div.onclick = function() {
+			console.log('message div', div.getAttribute('email'));
+			window.open('https://contacts.google.com/search/' + div.getAttribute('email'));
+		};
+	});
+}
+
 InboxSDK.load('2', 'sdk_inboxsdk-hlwrld_4616f707cc').then(function(sdk){
-	/*
-	sdk.Compose.registerComposeViewHandler(function(composeView){
-		// a compose view has come into existence, do something with it!
-		composeView.addButton({
-			title: "My Nifty Button!",
-			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
-			onClick: function(event) {
-				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
-			},
-		});
-	});
-	*/
-
-	// user clicked on a thread of messages
-	sdk.Conversations.registerThreadViewHandler(function(conversationsView){
-		/*
-		conversationsView.on('contactHover', function(evt) {
-			var email = evt.contact.emailAddress;
-			console.log('hover', email);
-			document.querySelectorAll('div[email=]')
-		});
-		*/
-		document.querySelectorAll('div[email]').forEach(function(div){
-			div.onclick = function() {
-				window.open('https://contacts.google.com/search/' + div.getAttribute('email'));
-			};
-		});
-		conversationsView.on('destroy', function(evt) {
-			console.log('destroy', conversationsView); // never happens, for some reason...
-		});
-	});
-
+	sdk.Conversations.registerMessageViewHandler(attachLinks);
 });
